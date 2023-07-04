61934ed571a177e063bd46d61fd04d79a190cef8
Tony Yang <tonyyanga@gmail.com>
2023-07-04 15:44:29 -0700

    rewrite

--- docs-proxy/next.config.js
+++ docs-proxy/next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          /*{
+  async rewrites() {
+    return [
+      /*{
             source: '/:slug*',
             destination: 'https://docs.cedalio.com/:slug*',
           },*/
-          {
-            source: '/:slug*',
-            destination: '/api/cedalio',
-          },
-        ]
+      {
+        source: "/",
+        destination: "/api/cedalio",
       },
-}
+      {
+        source: "/:slug*",
+        destination: "/api/cedalio",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
