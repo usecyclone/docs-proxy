d443adacf536cf0b8f29d8f155964a90c6e59ed1
Tony Yang <tonyyanga@gmail.com>
2023-07-07 22:13:31 -0700

    add cedalio demo site

--- docs-proxy/pages/api/cedalio.ts
+++ docs-proxy/pages/api/cedalio.ts
@@ -6,6 +6,7 @@ import axios from "axios";
 const proxyHosts: { [host: string]: string } = {
   "docs.cedalio.com": "https://github.cedalio.com",
   "cedalio.usecyclone.dev": "https://docs.cedalio.com",
+  "cedalio-docs-proxy.vercel.app": "https://gitbook.cedalio.com",
   "convex.usecyclone.dev": "https://docs.convex.dev",
   "continue.usecyclone.dev": "https://continue.dev",
 };
