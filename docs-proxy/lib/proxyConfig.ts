c3c1ee0455978425b584c88fa58166e85d7954f7
Tony Yang <tonyyanga@gmail.com>
2023-07-08 10:42:34 -0700

    quick fix

--- docs-proxy/lib/proxyConfig.ts
+++ docs-proxy/lib/proxyConfig.ts
@@ -1,7 +1,7 @@
 // the main site should use Vercel Edge Config
 // this is used for fallback
 export const proxyHosts: { [host: string]: string } = {
-  "docs.cedalio.com": "https://github.cedalio.com",
+  "docs.cedalio.com": "https://gitbook.cedalio.com",
   "cedalio.usecyclone.dev": "https://docs.cedalio.com",
   "cedalio-docs-proxy.vercel.app": "https://gitbook.cedalio.com",
   "convex.usecyclone.dev": "https://docs.convex.dev",
